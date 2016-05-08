5568f187f608ec67c2d175dc7cc0edfa590f21af
Christian Scheid <chris.scheid@gmail.com>
2016-05-08 14:16:36 +0100

    Add empty new shuffler

---
+++ lib/christian_shuffle.ex
@@ -0,0 +1,19 @@
+defmodule Karten.ChristianShuffle do
+	@behaviour Karten.Shuffler
+
+	def shuffle(decks) do
+		decks
+		# |> Enum.concat
+		# |> cut
+	end
+
+	#
+	# maciej's solution
+	#
+	# defp cut(deck) do
+	# 	:random.seed(:erlang.now())
+	# 	splitter = :random.uniform(length(deck))
+	# 	{top, bottom} = Enum.split(deck, splitter)
+	# 	bottom ++ top
+	# end
+end
